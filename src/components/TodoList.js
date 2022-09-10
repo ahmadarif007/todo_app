30e5abbfe1ffbdcd5e647edc70dc64607eb3aba1
Ahmad Arif <ahmadarif.0007@gmail.com>
2022-09-10 22:40:44 +0600

    Arif | develop | dynamic on todo list & todo item section

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,15 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import { useSelector } from "react-redux";
 
 export default function TodoList() {
+  const todos = useSelector((state) => state.todos);
+
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      <TodoItem />
+      {todos.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 }
